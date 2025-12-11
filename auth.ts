b4a8d757d42d962300afb01534ce7aaec1c463ee
Jithin7777 <133039735+Jithin7777@users.noreply.github.com>
2025-12-11 12:10:14 +0530

    chore: update prisma config, schema, and authentication setup

--- auth.ts
+++ auth.ts
@@ -1,7 +1,7 @@
 import NextAuth from "next-auth"
 import { PrismaAdapter } from "@auth/prisma-adapter"
 import authConfig from "./auth.config"
-import { PrismaClient } from "./lib/generated/prisma"
+import { PrismaClient } from "@prisma/client"
 
 const prisma = new PrismaClient()
  
--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,9 +1,17 @@
-import { withAccelerate } from "@prisma/extension-accelerate"
-import { PrismaClient } from "./generated/prisma"
+import { PrismaPg } from '@prisma/adapter-pg';
+import { PrismaClient } from "@/lib/generated/prisma";
 
-const globalForPrisma = globalThis as unknown as { prisma: PrismaClient }
- 
-export const prisma =
-  globalForPrisma.prisma || new PrismaClient().$extends(withAccelerate())
- 
-if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma
+const adapter = new PrismaPg({ 
+  connectionString: process.env.DATABASE_URL 
+});
+
+const prismaClientSingleton = () => {
+  return new PrismaClient({ adapter })
+}
+declare const globalThis: {
+  prismaGlobal: ReturnType<typeof prismaClientSingleton>;
+} & typeof global;
+const prisma = globalThis.prismaGlobal ?? prismaClientSingleton()
+
+export default prisma
+if (process.env.NODE_ENV !== 'production') globalThis.prismaGlobal = prisma
