18577789e38403379198069356040152b7592fbd
Jithin7777 <133039735+Jithin7777@users.noreply.github.com>
2025-12-16 21:05:43 +0530

    updated

--- features/components/auth/LogoutButton.tsx
+++ features/components/auth/LogoutButton.tsx
@@ -1,12 +1,36 @@
+// "use client";
+
+// import { Button } from "@/components/ui/button";
+// import { signOut } from "next-auth/react";
+
+// export default function LogoutButton() {
+//   return (
+//     <Button
+//       onClick={() => signOut({ callbackUrl: "/login" })}
+//       className="mt-4 bg-red-500 text-white px-4 py-2 rounded hover:bg-red-600"
+//     >
+//       Logout
+//     </Button>
+//   );
+// }
+
+
+
+
 "use client";
 
 import { Button } from "@/components/ui/button";
 import { signOut } from "next-auth/react";
 
 export default function LogoutButton() {
+  const callbackUrl =
+    process.env.NEXT_PUBLIC_AUTH_URL
+      ? `${process.env.NEXT_PUBLIC_AUTH_URL}/login`
+      : "/login"; // fallback for dev
+
   return (
     <Button
-      onClick={() => signOut({ callbackUrl: "/login" })}
+      onClick={() => signOut({ callbackUrl })}
       className="mt-4 bg-red-500 text-white px-4 py-2 rounded hover:bg-red-600"
     >
       Logout
