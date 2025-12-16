0a56299b5653f1493484575141156a63c76db858
Jithin7777 <133039735+Jithin7777@users.noreply.github.com>
2025-12-16 21:57:18 +0530

    updated

--- proxy.ts
+++ proxy.ts
@@ -26,31 +26,28 @@
 
 
 
-// middleware.ts
+// proxy.ts
 import { getToken } from "next-auth/jwt";
 import { NextRequest, NextResponse } from "next/server";
 
 export const config = {
-  matcher: ["/teacher/:path*", "/student/:path*"], // protect these routes
+  matcher: ["/teacher/:path*", "/student/:path*"],
 };
 
-export async function middleware(req: NextRequest) {
+export async function proxy(req: NextRequest) {
   const token = await getToken({ req, secret: process.env.AUTH_SECRET });
   const path = req.nextUrl.pathname;
 
-  // Production login URL
   const loginUrl = "https://smart-assignment-manager.vercel.app/login";
 
-  // Not logged in
   if (!token) return NextResponse.redirect(loginUrl);
-
-  // Teacher-only routes
   if (path.startsWith("/teacher") && token.role !== "TEACHER")
     return NextResponse.redirect(loginUrl);
-
-  // Student-only routes
   if (path.startsWith("/student") && token.role !== "STUDENT")
     return NextResponse.redirect(loginUrl);
 
   return NextResponse.next();
 }
+
+// Then export it as default for Next.js 16+ Proxy
+export default proxy;
