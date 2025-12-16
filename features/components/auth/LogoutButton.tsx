457620a93c02af69ebe6c6fe54b3d9f225ec7217
Jithin7777 <133039735+Jithin7777@users.noreply.github.com>
2025-12-16 21:49:35 +0530

    updated

--- features/components/auth/LogoutButton.tsx
+++ features/components/auth/LogoutButton.tsx
@@ -23,14 +23,11 @@ import { Button } from "@/components/ui/button";
 import { signOut } from "next-auth/react";
 
 export default function LogoutButton() {
-  const callbackUrl =
-    process.env.NEXT_PUBLIC_AUTH_URL
-      ? `${process.env.NEXT_PUBLIC_AUTH_URL}/login`
-      : "/login"; // fallback for dev
-
   return (
     <Button
-      onClick={() => signOut({ callbackUrl })}
+      onClick={() =>
+        signOut({ callbackUrl: "https://smart-assignment-manager.vercel.app/login" })
+      }
       className="mt-4 bg-red-500 text-white px-4 py-2 rounded hover:bg-red-600"
     >
       Logout
